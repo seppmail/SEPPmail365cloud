ea9e466faf6f5b184cc0e0b199a52fef4909c62b
Roman Stadlmair <stadlmair@seppmail.at>
2022-08-21 16:43:49 +0200

    #47 ARC header mod optimized.

--- Public/Connectors.ps1
+++ Public/Connectors.ps1
@@ -284,9 +284,18 @@ function New-SC365Connectors
 
         Write-Verbose "Add ARC-Signature for seppmail.cloud"
         try {
-            [string[]]$ath = (Get-ArcConfig).ArctrustedSealers
-            if ($ath.Contains('SEPPmail.cloud')) {
+            [string]$ath = (Get-ArcConfig).ArctrustedSealers
+            if ((!($ath.Contains('SEPPmail.cloud'))) -or (!($ath.Length))) {
+                Write-Verbose "ARC header $ath exists but does not contain SEPPmail.cloud, adding SEPPmail.cloud"
+                [string]$athNew = $ath + ',SEPPmail.cloud'
+                Set-ArcConfig -Identity default -ArcTrustedSealers $athNew|Out-Null
+            }
+            if (!($ath)) {
+                Write-Verbose "ARC header is empty, adding SEPPmail.cloud"
                 Set-ArcConfig -Identity default -ArcTrustedSealers 'SEPPmail.cloud'|Out-Null
+            }
+            if (($ath.Contains('SEPPmail.cloud'))) {
+                Write-Verbose "ARC header $ath exists and contains SEPPmail.cloud, no action required"
             }
         } catch {
             throw [System.Exception] "Error: $($_.Exception.Message)"
